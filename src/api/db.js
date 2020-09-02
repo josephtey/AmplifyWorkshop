790c38b673709e73285a6cea6f06f5b7761dbc6a
EC2 Default User <ec2-user@ip-172-31-24-114.ap-southeast-2.compute.internal>
2020-09-02 11:19:07 +0000

    comments

--- src/api/db.js
+++ src/api/db.js
@@ -1,7 +1,14 @@
+// This function is called immediately when the page loads, before populating the table with this data
 export async function getUserItems() {
     return []
 }
 
-export async function addItem(itemName) {}
+// This function is called when a user clicks the button 'Add'
+export async function addItem(itemName) {
+    
+}
 
-export async function deleteItem(timestamp) {}
+// This function is called when a user deletes an existing item in the table
+export async function deleteItem(timestamp) {
+    
+}
